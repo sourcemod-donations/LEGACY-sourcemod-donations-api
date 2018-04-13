3f89df771280ebf22aad6a10204088ffd7299896
kbkk <sejntcik@gmail.com>
2018-04-14 01:19:52 +0200

    make steam auth verify route return proper response code on failure and make adaptations for dev environment
    steam openid realm is now set to a hardcoded value, would be good to make it configurable (and for any environment) as some people might want to host frontend and backend on different domains

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AuthController
@@ -23,17 +24,29 @@ class AuthController
      * @var UrlGeneratorInterface
      */
     private $urlGenerator;
+    /**
+     * @var KernelInterface
+     */
+    private $kernel;
 
-    public function __construct(UserRepository $userRepository, UrlGeneratorInterface $urlGenerator)
+    public function __construct(UserRepository $userRepository, UrlGeneratorInterface $urlGenerator, KernelInterface $kernel)
     {
         $this->userRepository = $userRepository;
         $this->urlGenerator = $urlGenerator;
+        $this->kernel = $kernel;
     }
 
     private function createSteamAuthInstance(Request $request)
     {
         $baseUrl = $request->getSchemeAndHttpHost();
-        $returnTo = $this->urlGenerator->generate('auth_steam_verify');
+        $returnTo = 'steam_authenticate.html';
+
+        if($this->kernel->getEnvironment() === 'dev')
+        {
+            // probably should be configurable
+            // even when the env is not dev
+            $baseUrl = 'http://localhost:3000/';
+        }
 
         $steamAuth = new SteamOpenId([
             'realm' => $baseUrl,
@@ -53,12 +66,15 @@ class AuthController
     {
         $steamAuth = $this->createSteamAuthInstance($request);
         $steamid = $steamAuth->verifyAssertion($request->query->all());
-        $user = $this->userRepository->findBySteamid($steamid);
 
         if($steamid === false)
         {
-            throw new \Exception('Failed to authenticated with Steam');
+            return new JsonResponse([
+                'message' => 'Failed to authenticate with Steam'
+            ], Response::HTTP_UNAUTHORIZED);
         }
+
+        $user = $this->userRepository->findBySteamid($steamid);
 
         if(!$user)
         {
