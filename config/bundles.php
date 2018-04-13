741fe434a85008aefea24df0c779b7f788ee7e22
kbkk <sejntcik@gmail.com>
2018-04-13 17:29:42 +0200

    add nelmio cors for dev environment

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['dev' => true],
 ];
